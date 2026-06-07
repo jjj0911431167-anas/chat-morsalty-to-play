ea3a012761a286966e7d7daf6dec75e99ba86249
joncarl0093-ops <joncarl0093@gmail.com>
2026-06-07 14:49:26 +0200

    Add users endpoint

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'application/json'});
-  res.end(JSON.stringify({
-    status: "online",
-    app: "Chat Morsalty To Play"
-  }));
+
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type':'application/json'});
+    return res.end(JSON.stringify({
+      app: 'Chat Morsalty To Play',
+      status: 'online'
+    }));
+  }
+
+  if (req.url === '/users') {
+
+    const users = JSON.parse(
+      fs.readFileSync('./backend/database/users.json')
+    );
+
+    res.writeHead(200, {'Content-Type':'application/json'});
+    return res.end(JSON.stringify(users));
+  }
+
+  res.writeHead(404);
+  res.end('Not Found');
+
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log('Server running on port 3000');
 });
